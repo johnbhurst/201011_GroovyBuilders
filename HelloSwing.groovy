bf3764af54253ad0e74f22f1363c4f3c5f8c9406
John Hurst <john.b.hurst@gmail.com>
2010-11-22 09:43:13 +1300

    Use EDT; maybe a little simpler, following style in http://groovy.codehaus.org/Swing+Builder.

--- HelloSwing.groovy
+++ HelloSwing.groovy
@@ -1,12 +1,11 @@
 import groovy.swing.SwingBuilder
 
-def swing = new SwingBuilder()
-def frame = swing.frame(title: "Hello Swing!") {
-  hbox {
-    label(text: "Hello, Swing!")
-    button(text: "Click Me to close", actionPerformed: {System.exit(0)})
+new SwingBuilder().edt {
+  frame(title: "Hello Swing!", size: [400, 200], show: true) {
+    hbox {
+      label(text: "Hello, Swing!")
+      button(text: "Click Me to close", actionPerformed: {System.exit(0)})
+    }
   }
 }
-frame.pack()
-frame.show()
 
