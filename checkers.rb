76ceaea2cafce9a1f5d334ccfe644fb467d41f92
John Hurst <john.b.hurst@gmail.com>
2010-11-27 09:12:38 +1300

    Moved method defs into block; now reads pretty naturally.

--- checkers.rb
+++ checkers.rb
@@ -5,16 +5,13 @@ whitesquare = "fill:#cccccc;stroke:#000000;stroke-width:0.5;"
 blackpiece = "fill:#333333;stroke:#000000;"
 whitepiece = "fill:#999999;stroke:#000000;"
 
-class Builder::XmlMarkup
+Builder::XmlMarkup.new(target: STDOUT, indent: 2).instance_eval do
   def square(i, j, style)
     rect(width: 10, height: 10, x: 2 + 10 * j, y: 2 + 10 * i, style: style)
   end
   def piece(i, j, style)
     circle(cx: 2 + 5 + 10 * j, cy: 2 + 5 + 10 * i, r: 4, style: style)
   end
-end
-
-Builder::XmlMarkup.new(target: STDOUT, indent: 2).instance_eval do
   svg(xmlns: "http://www.w3.org/2000/svg", width: 84, height: 84) do
     g do
       0.upto(7) do |i|
