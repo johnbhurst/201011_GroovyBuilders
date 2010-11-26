2cd59440c4d6236b681dca59e516ad410b04585e
John Hurst <john.b.hurst@gmail.com>
2010-11-27 09:07:23 +1300

    Refactored square, piece logic into methods/blocks.

--- checkers.groovy
+++ checkers.groovy
@@ -6,19 +6,21 @@ def blackpiece = "fill:#333333;stroke:#000000;"
 def whitepiece = "fill:#999999;stroke:#000000;"
 
 new MarkupBuilder().svg(xmlns: "http://www.w3.org/2000/svg", width: 84, height: 84) {
+  def square = {i, j, style -> rect(width: 10, height: 10, x: 2 + 10 * j, y: 2 + 10 * i, style: style)}
+  def piece = {i, j, style -> circle(cx: 2 + 5 + 10 * j, cy: 2 + 5 + 10 * i, r: 4,  style: style)}
   g {
     for (i in 0..7) {
       for (j in 0..7) {
         if (i % 2 == j % 2) {
-          rect(width: 10, height: 10, x: 2 + 10 * j, y: 2 + 10 * i, style: whitesquare)
+          square(i, j, whitesquare)
         }
         else {
-          rect(width: 10, height: 10, x: 2 + 10 * j, y: 2 + 10 * i, style: blacksquare)
+          square(i, j, blacksquare)
           if (i <= 2) {
-            circle(cx: 2 + 5 + 10 * j, cy: 2 + 5 + 10 * i, r: 4,  style: whitepiece)
+            piece(i, j, whitepiece)
           }
           if (i >= 5) {
-            circle(cx: 2 + 5 + 10 * j, cy: 2 + 5 + 10 * i, r: 4,  style: blackpiece)
+            piece(i, j, blackpiece)
           }
         }
       }
--- checkers.rb
+++ checkers.rb
@@ -5,20 +5,29 @@ whitesquare = "fill:#cccccc;stroke:#000000;stroke-width:0.5;"
 blackpiece = "fill:#333333;stroke:#000000;"
 whitepiece = "fill:#999999;stroke:#000000;"
 
+class Builder::XmlMarkup
+  def square(i, j, style)
+    rect(width: 10, height: 10, x: 2 + 10 * j, y: 2 + 10 * i, style: style)
+  end
+  def piece(i, j, style)
+    circle(cx: 2 + 5 + 10 * j, cy: 2 + 5 + 10 * i, r: 4, style: style)
+  end
+end
+
 b = Builder::XmlMarkup.new(target: STDOUT, indent: 2)
 b.svg(xmlns: "http://www.w3.org/2000/svg", width: 84, height: 84) do
   b.g do
     0.upto(7) do |i|
       0.upto(7) do |j|
         if i % 2 == j % 2 then
-          b.rect(width: 10, height: 10, x: 2 + 10 * j, y: 2 + 10 * i, style: whitesquare)
+          b.square(i, j, whitesquare)
         else
-          b.rect(width: 10, height: 10, x: 2 + 10 * j, y: 2 + 10 * i, style: blacksquare)
+          b.square(i, j, blacksquare)
           if i <= 2 then
-            b.circle(cx: 2 + 5 + 10 * j, cy: 2 + 5 + 10 * i, r: 4, style: whitepiece)
+            b.piece(i, j, whitepiece)
           end
           if i >= 5 then
-            b.circle(cx: 2 + 5 + 10 * j, cy: 2 + 5 + 10 * i, r: 4, style: blackpiece)
+            b.piece(i, j, blackpiece)
           end
         end
       end
